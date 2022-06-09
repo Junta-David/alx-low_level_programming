28b3eefa75eb768c387e47b0ef6aac7821a51c21
Junta-David <juntadavid@gmail.com>
2022-06-09 13:57:41 -0700

    Debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - positive or negative function
+ * @void: no arguments
+ * Return: 0
+ **/
+void positive_or_negative(int i)
+{
+	if(i > 0)
+	{
+		printf("%d is positve\n", i);
+	}
+	else if(i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else
+	{
+		printf("%d is negative\n", i);
+	}
+}
