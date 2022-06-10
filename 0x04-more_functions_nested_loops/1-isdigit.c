523183fa0a5004175018f671614141fbf44de514
Junta-David <juntadavid@gmail.com>
2022-06-10 05:44:56 -0700

    isdigit file

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - check for upper case character
+ * @c:integer
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
