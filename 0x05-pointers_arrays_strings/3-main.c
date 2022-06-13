e2f2cd4c0ea7ff2109ea2f12e52e95a3291ae3ba
Junta-David <juntadavid@gmail.com>
2022-06-13 13:40:36 -0700

    M

--- 0x05-pointers_arrays_strings/3-main.c
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - check the code
@@ -9,7 +8,7 @@
 int main(void)
 {
 	char *str;
-	
+
 	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
 	_puts(str);
 	return (0);
