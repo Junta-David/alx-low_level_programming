c158b74e938bd498e6a191a46657ad641c2acd17
Junta-David <juntadavid@gmail.com>
2022-06-23 02:43:38 -0700

    Recursion

--- 0x08-recursion/6-main.c
+++ 0x08-recursion/6-main.c
@@ -11,20 +11,20 @@ int main(void)
 	int r;
 
 	r = is_prime_number(1);
-	print("%d\n", r);
+	printf("%d\n", r);
 	r = is_prime_number(1024);
-	print("%d\n", r);
+	printf("%d\n", r);
 	r = is_prime_number(16);
-	print("%d\n", r);
+	printf("%d\n", r);
 	r = is_prime_number(17);
-	print("%d\n", r);
+	printf("%d\n", r);
 	r = is_prime_number(25);
-	print("%d\n", r);
+	printf("%d\n", r);
 	r = is_prime_number(-1);
-	print("%d\n", r);
+	printf("%d\n", r);
 	r = is_prime_number(113);
-	print("%d\n", r);
+	printf("%d\n", r);
 	r = is_prime_number(7919);
-	print("%d\n", r);
+	printf("%d\n", r);
 	return (0);
 }
